73ecf432b3ea47a378cf8b8f4ba80d683eb7cbc7
Namit Nayak <namitnayak@gmail.com>
2016-07-29 02:20:50 -0400

    Code Commit for Async call of image downloading

---
+++ MedRep/MedRep/Common/UIImage+Helpers.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Helpers.h
+//  MedRep
+//
+//  Created by Namit Nayak on 7/29/16.
+//  Copyright © 2016 MedRep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage_Helpers : UIImage
+
+@end
---
+++ MedRep/MedRep/Common/UIImage+Helpers.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Helpers.m
+//  MedRep
+//
+//  Created by Namit Nayak on 7/29/16.
+//  Copyright © 2016 MedRep. All rights reserved.
+//
+
+#import "UIImage+Helpers.h"
+
+@implementation UIImage_Helpers
+
+@end
