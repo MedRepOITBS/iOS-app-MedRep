ad1aea3e8f9a45f46de3a31fa05d17fa59959ccb
Namit Nayak <namitnayak@gmail.com>
2016-07-29 01:13:08 -0400

    Code Commit adding new images

--- MedRep/MedRep/Communication/MRWebserviceHelper.m
+++ MedRep/MedRep/Communication/MRWebserviceHelper.m
@@ -213,7 +213,24 @@
     self.serviceType = kMRWebServiceTypeAddMemberToGroup;
     [self sendServiceRequest:urlRequest withHandler:responceHandler];
 }
-
+-(void)addWorkExperience:(NSDictionary *)reqDict withHandler:(completionHandler)responceHandler{
+    
+    NSString *stringFormOfUrl = [NSString stringWithFormat:@"%@/medrep-web/addContacts?token=%@",kHostName,[MRDefaults objectForKey:kAuthenticationToken]];
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:reqDict
+                                                       options:NSJSONWritingPrettyPrinted
+                                                         error:nil];
+    NSURL *url = [NSURL URLWithString:stringFormOfUrl];
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:url];
+    [urlRequest setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [urlRequest setTimeoutInterval:120];
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [urlRequest setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[jsonData length]] forHTTPHeaderField:@"Content-Length"];
+    [urlRequest setHTTPBody: jsonData];
+    self.serviceType = kMRWebServiceTypeAddMember;
+    [self sendServiceRequest:urlRequest withHandler:responceHandler];
+}
 - (void)addMembers:(NSDictionary *)reqDict withHandler:(completionHandler)responceHandler{
     NSString *stringFormOfUrl = [NSString stringWithFormat:@"%@/medrep-web/addContacts?token=%@",kHostName,[MRDefaults objectForKey:kAuthenticationToken]];
     
