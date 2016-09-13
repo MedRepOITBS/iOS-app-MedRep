35589064c690bd1e5696e9db30746afbf5b00e18
Vamsi <vkatragadda@sapient.com>
2016-09-13 23:22:49 +0100

    Bug Fix - Notification View

--- MedRep/MedRep/Notifications/MRNotificationsDetailsViewController.m
+++ MedRep/MedRep/Notifications/MRNotificationsDetailsViewController.m
@@ -54,6 +54,11 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self.navigationController setNavigationBarHidden:NO];
+}
 
 - (void)loadNotificationDetails
 {
